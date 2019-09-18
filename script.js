ab53a5012937941ce8162472e45f8030259dbe4e
MorganeBeCode <morgane.jansen@gmail.com>
2019-09-18 16:16:58 +0200

    update

--- script.js
+++ script.js
@@ -5,10 +5,33 @@ let multiplier = document.querySelector("#multiplier");
 let multiplicateur = 1;
 let autoclic = document.querySelector("#autoclic");
 let bonus = document.querySelector("#bonus");
+let autoclicPurchase = false;
+let y = 30;
+
+function check() {
+    if (score < (50 * multiplicateur)) {
+        multiplier.disabled = true;
+    } else {
+        multiplier.disabled = false;
+    }
+    if (score < 500 || autoclicPurchase == true) {
+        autoclic.disabled = true;
+    } else {
+        autoclic.disabled = false;
+    }
+    if (score < 5000 || y < 30) {
+        bonus.disabled = true;
+    } else {
+        bonus.disabled = false;
+    }
+
+}
+check();
 
 clic.addEventListener("click", function () {
     score += multiplicateur;
     affichage.innerHTML = score;
+    check();
 })
 
 function augmenterMultiplicateur() {
@@ -19,37 +42,59 @@ function augmenterMultiplicateur() {
 }
 
 autoclic.addEventListener("click", function () {
-    if (score >= 500) {
-        setInterval(function () { score += multiplicateur; affichage.innerHTML = score; }, 1000);
-        autoclic.style.display = "none";
-        let info = document.createElement("span");
-        info.innerHTML = "Autoclick acheté";
-        info.setAttribute("id", "info");
-        document.querySelector(".grid-container").appendChild(info);
-    }
+    autoclicPurchase = true;
+    setInterval(function () { check(); score += multiplicateur; affichage.innerHTML = score; }, 1000);
+    autoclic.disabled = true;
+    autoclic.innerHTML = "Autoclick acheté";
+    check();
 })
 
 multiplier.addEventListener("click", function () {
     augmenterMultiplicateur();
-    multiplier.innerHTML = `Multiplicateur x${multiplicateur} <br /> coût : ${50 * multiplicateur}`;
+    multiplier.innerHTML = `Multiplicateur x${(multiplicateur) + 1} <br /> coût : ${50 * multiplicateur}`;
     affichage.innerHTML = score;
+    check();
 })
 
-let y = 30;
+function acheterBonus() {
+    let interval = setInterval(function () {
+        check();
+        score += (multiplicateur * 2);
+        bonus.innerHTML = y;
+        affichage.innerHTML = score;
+        bonus.disabled = true;
+        y--;
+        if (y == -1) {
+            clearInterval(interval);
+            bonus.innerHTML = "Acheter bonus 200% <br /> Coût : 5000";
+            bonus.disabled = false;
+            y = 30;
+        }
+    }, 1000);
+
+
+}
 bonus.addEventListener("click", function () {
     if (score >= 5000) {
-        let interval = setInterval(function () {
-            score += (multiplicateur * 2);
-            y--;
-            affichage.innerHTML = score;
-            bonus.innerHTML = y
-        }, 1000);
-
-        setTimeout(clearInterval(interval), 30000);
+        acheterBonus();
     }
 })
 
-document.querySelector("#cheat").addEventListener("click", function () {
-    score += 1000;
-    affichage.innerHTML = score;
+let cheat = document.querySelector("#cheat");
+
+cheat.addEventListener("click", function () {
+    let mdp = prompt("Quel est le mot-de-passe ?");
+    if (mdp == "bazinga") {
+        document.querySelector(".grid-container").removeChild(cheat);
+        let cheat2 = document.createElement("button");
+        cheat2.setAttribute("id", "cheat2");
+        cheat2.innerHTML = "cheat";
+        document.querySelector(".grid-container").insertBefore(cheat2, autoclic);
+        cheat2.addEventListener("click", function () {
+            score += 1000;
+            affichage.innerHTML = score;
+            check();
+        })
+    }
 })
+
